bcb67e747bc3d688f017f47c44dac222b3e13438
justinjarrard <jjarrard21@gmail.com>
2021-03-15 11:25:07 -0400

    push

--- Develop/routes/noteRoutes.js
+++ Develop/routes/noteRoutes.js
@@ -27,4 +27,18 @@ router.post('/notes', (req, res) => {
     })
   })
 })
+
+router.delete('/notes/:id', (req, res) => {
+  let id = req.params.id
+  fs.readFile(join(__dirname, '..', 'db', 'db.json'), 'utf8', (err, data) => {
+    if (err) { console.log(err) }
+    let notes = JSON.parse(data)
+    notes = notes.filter(note => note.id !== id)
+    fs.writeFile(join(__dirname, '..', 'db', 'db.json'), JSON.stringify(notes),
+      err => {
+        if (err) { console.log(err) }
+        res.sendStatus(200)
+      })
+  })
+})
 module.exports = router
